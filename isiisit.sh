4baa019f7d1c5aa5ded1b600bc815560ae64cc9e
Jean-Olivier Irisson <irisson@normalesup.org>
2014-01-11 00:26:45 +0100

    First test of thresholding and particle analysis

--- isiisit.sh
+++ isiisit.sh
@@ -5,4 +5,5 @@
 #
 #
 
-time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISpreProcess.ISIISpreProcess $1 $2
+# time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISpreProcess.ISIISpreProcess $1 $2
+time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISgetParticles.ISIISgetParticles $1 $2 $3
--- src/ISIISgetParticles/ISIISgetParticles.java
+++ src/ISIISgetParticles/ISIISgetParticles.java
@@ -1,17 +1,155 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ISIISgetParticles;
+
+// file listing
+import java.io.File;
+import java.io.FilenameFilter;
+// utility functions
+import ISIISutils.Message;
+// ImageJ functionality
+import ij.IJ;
+import ij.ImagePlus;
+import ij.Prefs;
+import static ij.measure.Measurements.*;
+import ij.measure.ResultsTable;
+import ij.plugin.Duplicator;
+import ij.plugin.filter.Analyzer;
+import ij.plugin.filter.ParticleAnalyzer;
 
 /**
  *
  * @author jiho
  */
 public class ISIISgetParticles {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+
+    /**
+     * Here we:
+     * List all BMP files in the working directory
+     * Threshold each image
+     * Measure particles characteristics and store the result in a text file
+     * Extract an image containing each particle
+     *
+     * @param args the command line arguments
+     *        args[0] : source directory
+     *        args[1] : destination directory
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        // Options
+        //-------------------------------------------------------------------------
+        // print progress messages
+        final boolean verbose = true;
+        // print debug messages
+        final boolean debug = true;
+
+
+        // Read arguments
+        //-------------------------------------------------------------------------
+        if ( args.length != 3 ) {
+            Exception e = new Exception("Not enough arguments");
+            throw e;
+        }
+        // first argument: working directory (where the BMP files are)
+        String workDirName = args[0];
+        // second argument: destination directory (where the results should be written)
+        String destDirName = args[1];
+        String thresholdString = args[2];
+        int threshold = Integer.parseInt(thresholdString);
+        if ( debug ) { Message.debug("threshold = " + threshold); }
+
+        // List BMP files in working directory
+        //-------------------------------------------------------------------------
+        // create filename filter for bmp files
+        FilenameFilter bmpFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if(name.lastIndexOf('.')>0) {
+                    // get extension
+                    int lastIndex = name.lastIndexOf('.');
+                    String str = name.substring(lastIndex);
+                    // match bmp extension
+                    if(str.equals(".bmp")) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+        // convert workding directoy name to Java File object
+        File workDir = new File(workDirName);
+        // list all bmp files
+        File[] bmpFiles = workDir.listFiles(bmpFilter);
+        if ( bmpFiles == null ) {
+            Exception e = new Exception("Cannot list bmp files in " + workDir.getAbsolutePath() );
+            throw e;
+        }
+        int nbOfFiles = bmpFiles.length;
+        if ( verbose ) { System.out.println("Processing " + nbOfFiles + " bmp images"); }
+
+        // Create the destination directory, if it does not exist already
+        File destDir = new File(destDirName);
+        if ( ! destDir.exists() ) {
+            boolean success = destDir.mkdir();
+            if ( ! success ) {
+                Exception e = new Exception("Cannot create destination directory");
+                throw e;
+            }
+        }
+
+        // Loop over all images
+        //-------------------------------------------------------------------------
+        for (File file:bmpFiles) {
+
+            String fileName = file.getName();
+            if ( verbose ) { System.out.println(fileName); }
+
+            // open file
+    		ImagePlus imp = IJ.openImage(file.getAbsolutePath());
+
+            // remove extension
+            String fileNameNoExt = fileName.substring(0, fileName.lastIndexOf('.'));
+            // prepare destination
+            String outName = destDirName + fileNameNoExt;
+            if ( debug ) { Message.debug("outName = " + outName); }
+            // save a copy of the original image
+            // if ( debug ) { IJ.save(imp, outName + "-0-orig.png"); }
+
+
+            // threshold image
+    		ImagePlus imp2 = new Duplicator().run(imp);
+            IJ.setThreshold(imp2, 0, threshold);
+            // TODO: set a fixed threshold. It seems something around 160-200 combined together with a large minimum particle size (several hundrer pixels) is a good compromize to keep transparent organisms as one particle and not be polluted by small particles
+            // IJ.setAutoThreshold(imp, "MaxEntropy");
+            // TODO: seems good but deserves more testing. Is much longer to compute
+            Prefs.blackBackground = false;
+            IJ.run(imp2, "Convert to Mask", "");
+            if ( debug ) { IJ.save(imp2, outName + "-1-mask.png"); }
+
+
+            // analyse particles
+            // GUI scripting version
+            IJ.run("Set Measurements...", "area mean standard modal min centroid center perimeter bounding fit shape feret's integrated median skewness kurtosis area_fraction stack redirect=None decimal=3");
+            Analyzer.setRedirectImage(imp);
+            IJ.run(imp2, "Analyze Particles...", "size=400-Infinity circularity=0.00-1.00 show=Nothing exclude clear");
+            IJ.saveAs("Results", outName + ".txt");
+
+            // analyse particles
+            // programmatic version
+            ResultsTable rt = new ResultsTable();
+            rt.reset();
+            int options = 0; // set all ParticleAanalyzer options to false
+            int measurements = AREA+MEAN+CENTROID;
+            // TODO: set more measurements
+            ParticleAnalyzer pa = new ParticleAnalyzer(options, measurements, rt, 400, 1000000);
+            // TODO: find a better definition of "infinity"
+            Analyzer.setRedirectImage(imp);
+            pa.analyze(imp2);
+            int nParticles = rt.getCounter();
+            if ( verbose ) { System.out.println("found " + nParticles + " particles"); }
+            rt.saveAs(outName + ".csv");
+
+        }
+
     }
+
 }
