b03e0a55f1fb0406581517eef89e7dba52cae374
Jean-Olivier Irisson <irisson@normalesup.org>
2014-01-16 12:33:48 +0100

    Allow ImageJ to run in headless mode
    The IJ.run command creates requests to the interface, even if it is
    not displayed. For such requests to work, a X server must be running on
    Linux. This makes running the code remotely cumbersome and possibly slower.
    
    Here we run the code winthin a fake X11 server (Xfvb).
    TODO: need to check that it works in OS X.

--- isiisit.sh
+++ isiisit.sh
@@ -5,5 +5,13 @@
 #
 #
 
-# time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISpreProcess.ISIISpreProcess $1 $2
-time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISgetParticles.ISIISgetParticles $1 $2 $3
+export DISPLAY=:1
+Xvfb $DISPLAY -auth /dev/null & (
+
+time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp libs/ij.jar:dist/ISIISit.jar ISIISpreProcess.ISIISpreProcess $1 $2
+# time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISgetParticles.ISIISgetParticles $1 $2 $3
+
+wait
+)
+
+exit 0
