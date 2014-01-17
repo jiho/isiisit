4d4e51d9a37a233eac6f21b752c7fa421885837d
Robin Faillettaz <faillettaz@hjort.(none)>
2014-01-17 18:53:11 +0100

    Change isiisit.sh back without Xvfb

--- isiisit.sh
+++ isiisit.sh
@@ -5,13 +5,13 @@
 #
 #
 
-export DISPLAY=:1
-Xvfb $DISPLAY -auth /dev/null & (
+#export DISPLAY=:1
+#Xvfb $DISPLAY -auth /dev/null & (
 
 time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp libs/ij.jar:dist/ISIISit.jar ISIISpreProcess.ISIISpreProcess $1 $2
 # time java -Xms32m -Xmx2000m -Xincgc -XX:+DisableExplicitGC -cp dist/lib/ij.jar:dist/ISIISit.jar ISIISgetParticles.ISIISgetParticles $1 $2 $3
 
-wait
-)
+#wait
+#)
 
-exit 0
+#exit 0
